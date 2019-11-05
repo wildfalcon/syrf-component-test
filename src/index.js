23a4cfb751a60bb1de64951df239b7f5ad075cdd
Laurie Young <laurie@wildfalcon.com>
2019-11-05 11:43:43 +0000

    Attempt (and fail) to import Simple Web Component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
-import {SimpleModule} from '@wildfalcon/graph-displayer';
+import {SimpleModule, SimpleWebComponent} from '@wildfalcon/graph-displayer';
 console.log(SimpleModule)
+console.log(SimpleWebComponent)
+
+window.customElements.define('simple-web-component', SimpleWebComponent);  
+
 // if (!customElements.get('graph-displayer')) {
 //     window.customElements.define('graph-displayer', GraphDisplayerWebComponent);  
 //   }

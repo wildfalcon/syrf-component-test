aeca854d5159e882f10c43504c498a45c6d0a96a
Laurie Young <laurie@wildfalcon.com>
2019-11-05 10:41:55 +0000

    Simple Module Import

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import '@wildfalcon/graph-displayer'
+import {SimpleModule} from '@wildfalcon/graph-displayer';
+console.log(SimpleModule)
 // if (!customElements.get('graph-displayer')) {
 //     window.customElements.define('graph-displayer', GraphDisplayerWebComponent);  
 //   }

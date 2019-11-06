7f5213d8f57a4f2213066912c45d36ffe71ad5fc
Laurie Young <laurie@wildfalcon.com>
2019-11-06 11:55:24 +0000

    Include graph-displayer

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import {SimpleModule, SimpleWebComponent} from '@wildfalcon/graph-displayer';
-console.log(SimpleModule)
-console.log(SimpleWebComponent)
-
-window.customElements.define('simple-web-component', SimpleWebComponent);  
-
-// if (!customElements.get('graph-displayer')) {
-//     window.customElements.define('graph-displayer', GraphDisplayerWebComponent);  
-//   }
